84f1b74c49f1eb303e4a988c3c18dbe27443105b
Kelly Su <z5418860@ad.unsw.edu.au>
2024-02-24 23:01:55 +1100

    backend

--- backend/py/task.py
+++ backend/py/task.py
@@ -13,21 +13,69 @@ class File:
 Task 1
 """
 def leafFiles(files: list[File]) -> list[str]:
-    return []
+    parents = []
+    ans = []
+    for file in files: 
+        if file.parent not in parents:
+            parents.append(file.parent)
+    for file in files:
+        if file.id not in parents:
+            ans.append(file.name)
+
+    return ans
 
 
 """
 Task 2
 """
 def kLargestCategories(files: list[File], k: int) -> list[str]:
-    return []
+    category_map = {}; 
+    for file in files:
+        for category in file.categories:
+            if category not in category_map:
+                category_map[category]= 1
+            else:
+                category_map[category] += 1
+
+    sorted_files = sorted(category_map.items(), key=lambda x: (-x[1], x[0]))
+
+    return [category[0] for category in sorted_files[:k]]
+
 
 
 """
 Task 3
 """
+
+def recurse(adjacency, file):
+    if file.id not in adjacency or len(adjacency[file.id]) == 0:
+        return file.size
+
+    total = 0
+    for f in adjacency[file.id]:
+        total += recurse(adjacency, f)
+    
+    return total
+        
+
+
 def largestFileSize(files: list[File]) -> int:
-    return 0
+    mp = {}
+    for file in files:
+        if file.parent in mp:
+            mp[file.parent].append(file)
+        else:
+            mp[file.parent] = [file]
+    
+    ans = 0
+    for file in files:
+        ans = max(ans, recurse(mp, file))
+
+    
+    return ans
+
+
+
 
 
 if __name__ == '__main__':
@@ -45,6 +93,7 @@ if __name__ == '__main__':
         File(144, "Spreadsheet2.xlsx", ["Documents", "Excel"], 3, 2048),
         File(233, "Folder3", ["Folder"], -1, 4096),
     ]
+
 
     assert sorted(leafFiles(testFiles)) == [
         "Audio.mp3",
